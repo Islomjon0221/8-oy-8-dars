c0a15f4823b59c1ca055bf666abd0e4d2d955f4a
Islomjon0221 <islomjon20110221@gmail.com>
2024-05-24 18:22:54 +0500

    VAZIFA TAYYOR

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Chart } from "./components/LineChart";
+
+function App() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        margin: "0 auto",
+      }}
+    >
+      <Chart></Chart>
+    </div>
+  );
+}
+
+export default App;
